5b5bcebbc85f36e2c930dea26882927814f54a1e
Aso2001159 <83046888+Aso2001159@users.noreply.github.com>
2021-07-30 00:42:14 +0900

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,24 +1,23 @@
-package ship_game;
 import java.util.Random;
 public class Ship {
     public String name;
     public int hp=3;
-    public int[][] position= new int [5][5]; 
+    public static int[][] position= new int [5][5]; 
     
-    public final int[][] st = {{0,1,0},{0,1,0}};
+    public final int[][] st = {{0,1,0},{0,1,0}};//攻撃判定
     
     Random r =new Random();
-    int randomX = 1+ r.nextInt(4);
-    int randomY = 1+ r.nextInt(4);
-    int cnt = 3;
-   public Ship(String name,int randomX,int randomY, int hp) {
+    int randomX = 1+ r.nextInt(4); //船位置決めX
+    int randomY = 1+ r.nextInt(4); //船位置決めY
+    int flg = 1;
+   Ship(String name,int randomX,int randomY, int hp) {//コンストラクタによる値の初期化
         this.name = name;
         this.hp = hp;
-        while(cnt==0) {
+        while(flg!=0) {
         	if(position[randomX][randomY]!=1) {
-        		this.position[randomX][randomY] = 1 ;//X,Y座標に位置である1を設定
-        		System.out.println(this.position[randomX][randomY]+this.name);
-        		cnt--;
+        		position[randomX][randomY] = 1 ;//X,Y座標に位置である1を設定
+        		System.out.println(randomX+" "+randomY+this.name);
+        		flg--;
         	} else {
         		randomX = 1+ r.nextInt(4);
         		randomY = 1+ r.nextInt(4);
@@ -33,12 +32,17 @@ public class Ship {
     public int getXY(int x,int y) { //値取り出し
     	return position[x][y];
     }
-    
-    public void shoot(int[][] input,int x,int y) { //入力値と船の位置が同じならメソッド実行。近いならニアミスメソッド実行。
-    	if(input[x][y]==getXY(x,y)){
-    		hit(this.hp);
+
+    public void shoot(int x,int y) { 
+        //入力値と船の位置が同じならメソッド実行。近いならニアミスメソッド実行。
+    	if(position[x][y]==1&&getXY(x,y)==1){
+
+    		hit(this.hp,x,y);
+
     	} else if(st[0][1]==getXY(x,y)||st[1][0]==getXY(x,y)||st[1][2]==getXY(x,y)||st[2][1]==getXY(x,y)) {
-    		nearmiss(x,y,this.name);	
+    		
+            nearmiss(x,y,this.name);
+
     	} else {
     		Game.miss();
     	}
@@ -61,10 +65,17 @@ public class Ship {
     	}
     }
     
-    public void hit(int hp) {
+    public void hit(int hp,int x,int y) {
     	if(this.hp>0) {
     		this.hp -=1;
-    		System.out.printf("&sにヒット残り%d",this.name,this.hp);
+    		System.out.printf("%sにヒット残り%d\n\n",this.name,this.hp);
+                randomX = 1+ r.nextInt(4);
+        		randomY = 1+ r.nextInt(4);
+                position[x][y]=0;
+                position[randomX][randomY]=1;
+            if(this.hp==0){
+                System.out.printf("%s撃沈\n",this.name);
+            }
     	}else {
     		Game.miss();
     	}
